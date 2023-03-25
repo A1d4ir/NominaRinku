8effcdc2df89b7ea9ff6b8ce95974a737bbf8f2d
ProdynamicsDevEcom <devecom@prodynamics.com.mx>
2023-03-25 14:01:41 -0600

    Add project files.

---
+++ NominaRinku.Domain/Empleado.cs
@@ -0,0 +1,11 @@
+﻿using NominaRinku.Domain.Common;
+
+namespace NominaRinku.Domain
+{
+    public class Empleado: BaseDomainModel
+    {
+        public int Numero { get; set; }
+        public string? Nombre { get; set; }
+        public Enum Rol { get; set; }
+    }
+}
