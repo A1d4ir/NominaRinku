14302ec689f29dfec507e942dd0c2cd07fe45e27
Aldair <aldair_fime@hotmail.com>
2023-03-26 18:14:18 -0600

    Capa de infraestructura desarrrollada

---
+++ NominaRinku.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NominaRinku.Infrastructure.Persistence;
+using NominaRinku.Infrastructure.Repositories;
+using NominaRinlu.Application.Contracts.Persistense;
+
+namespace NominaRinku.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<NominaRinkuDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("ConnectionString"))
+            );
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+
+            services.AddScoped<IEmpleadoRepository, EmpleadoRepository>();
+            services.AddScoped<ISueldoRepository, SueldoReporitory>();
+
+            return services;
+        }
+    }
+}
